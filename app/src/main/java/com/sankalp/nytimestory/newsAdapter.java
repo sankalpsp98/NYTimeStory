1729370ed33a9a75133ff355d33c6039a247f799
sankalp <sankalpsp98@gmail.com>
2019-07-01 22:27:29 +0530

    adapter for RecycleView

--- app/src/main/java/com/sankalp/nytimestory/newsAdapter.java
+++ app/src/main/java/com/sankalp/nytimestory/newsAdapter.java
@@ -1,4 +1,59 @@
 package com.sankalp.nytimestory;
 
-public class newsAdapter  {
+import android.app.LauncherActivity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class newsAdapter  extends RecyclerView.Adapter<newsAdapter.ViewHolder> {
+
+    private List<results> NewsList;
+    Context context;
+
+    public newsAdapter(List<results> NewsList, Context context) {
+        this.NewsList = NewsList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.news_list,viewGroup,false);
+
+        return  new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int i) {
+        results  results = NewsList.get(i);
+        holder.title.setText(results.getTitle());
+        holder.abstrac.setText(results.getAbstrac());
+        holder.by.setText(results.getByline());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return NewsList.size();
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView title;
+        TextView abstrac;
+        TextView by;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.textView2);
+            abstrac= itemView.findViewById(R.id.textView3);
+            by=itemView.findViewById(R.id.textView4);
+        }
+    }
 }
